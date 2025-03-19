c1cd705ad0d3b53bca882c0a3e8d2c9229d309d5
carrot-hu23 <1762858544@qq.com>
2025-03-19 13:20:50 +0800

    增加自定义物品给予按钮

--- src/pages/TooManyItemsPlus/OtherIOrder.tsx
+++ src/pages/TooManyItemsPlus/OtherIOrder.tsx
@@ -1,7 +1,10 @@
 import {sendCommandApi} from "../../api/commdApi.ts";
-import {Button, message, Space} from "antd";
+import {Button, Divider, Image, Input, InputNumber, message, Select, Space, Typography} from "antd";
 import {useParams} from "react-router-dom";
 import {ProCard} from "@ant-design/pro-components";
+import {useState} from "react";
+import {usePlayerListStore} from "../../store/usePlayerListStore.tsx";
+import {dstRoles} from "../../types/dst.ts";
 
 export default function OtherIOrder(){
 
@@ -18,9 +21,28 @@ export default function OtherIOrder(){
             })
     }
 
+    const [kuId, setKuId] = useState<string>();
+    const [amount, setAmount] = useState<number>(1);
+    const [prefab, setPrefab] = useState<string>();
+    const playerList = usePlayerListStore((state) => state.playerList);
+
+    function give(prefab: string, amount: number, kuId: string) {
+        const command = `ThePlayer = UserToPlayer(\\"${kuId}\\")   c_give(\\"${prefab}\\", ${amount}) ThePlayer = nil`
+        sendCommandApi(cluster || "", "Master", command)
+            .then(resp =>{
+                if (resp.code === 200) {
+                    message.success("发送成功")
+                } else {
+                    message.warning("发送失败")
+                }
+            })
+    }
+
     return(
         <ProCard>
-            <br/>
+            <Typography.Title level={5}>
+                时间按钮
+            </Typography.Title>
             <Space wrap size={16}>
                 <Button type={'primary'} onClick={() => send('TheWorld:PushEvent(\\"ms_nextcycle\\")')}>跳过一天</Button>
                 <Button type={'primary'} onClick={() => send('TheWorld:PushEvent(\\"ms_nextphase\\")')}>跳过时钟</Button>
@@ -37,8 +59,43 @@ export default function OtherIOrder(){
                 <Button type={'primary'}
                         onClick={() => send('TheWorld:PushEvent(\\"ms_forceprecipitation\\", false)')}>停止下雨</Button>
             </Space>
-            <br/>
-            <br/>
+            <Divider />
+            <Typography.Title level={5}>
+                自定义模组物品给予
+            </Typography.Title>
+            <Space size={16} wrap>
+                <Select
+                    defaultValue={kuId}
+                    style={{ width: 120 }}
+                    options={playerList.map(player => ({
+                        value: player.kuId,
+                        label: player.name
+                    }))}
+                    onChange={value => setKuId(value)}
+                />
+                <InputNumber
+                    style={{ width: 120 }}
+                    min={1}
+                    max={10}
+                    defaultValue={1}
+                    onChange={value => setAmount(value || 1)}
+                    addonAfter="数量"
+                />
+                {kuId && (
+                    <>
+                        day: {playerList.find(player => player.kuId === kuId)?.day || 0}
+                        <Image
+                            preview={false}
+                            width={48}
+                            src={dstRoles[`${playerList.find(player => player?.kuId === kuId)?.role}`] || dstRoles.mod}
+                        />
+                    </>
+                )}
+                <Input placeholder={'请输入物品代码'} onChange={value => setPrefab(value.target.value)} />
+                <Button type={'primary'} onClick={()=>{
+                    give(prefab||'', amount, kuId||'')
+                }} >发送</Button>
+            </Space>
         </ProCard>
     )
 }
