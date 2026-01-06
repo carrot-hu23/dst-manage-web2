45eaee65ae7938f883ff82895f1a0cc9c053abbb
huhuhu23 <1762858544@qq.com>
2026-01-06 09:33:44 +0800

    fix: remove js file

--- src/pages/Begin/component/Welcome.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var jsx_runtime_1 = require("react/jsx-runtime");
-var react_i18next_1 = require("react-i18next");
-var Welcome = function () {
-    var t = (0, react_i18next_1.useTranslation)().t;
-    return ((0, jsx_runtime_1.jsxs)(jsx_runtime_1.Fragment, { children: [(0, jsx_runtime_1.jsx)("h3", { children: t('Welcome to dst-admin-web management platform') }), (0, jsx_runtime_1.jsx)("div", { children: (0, jsx_runtime_1.jsx)("img", { src: "/assets/illustrations/illustration_login3.png", alt: "login" }) })] }));
-};
-exports.default = Welcome;
